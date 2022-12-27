6e3a087d92921ff907f637e18b4c72369d438531
jiro <johnjiro1114@gmail.com>
2022-12-27 23:15:54 +0900

    buffer実装"

---
+++ src/buffer.rs
@@ -0,0 +1,55 @@
+use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
+use std::io;
+use std::ops::{Index, IndexMut};
+use std::rc::Rc;
+
+use create::disk::{DiskManager, PageId, PAGE_SIZE};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error("no free buffer available in buffer pool")]
+    NoFreeBuffer,
+}
+
+#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct BufferId(usize);
+
+pub type Page = [u8; PAGE_SIZE];
+
+#[derive(Debug)]
+pub struct Buffer {
+    pub page_id: PageId,
+    pub page: RefCell<Page>,
+    pub is_dirty: Cell<bool>,
+}
+
+impl Default for Buffer {
+    fn default() -> Self {
+        Self {
+            page_id: Default::default(),
+            page: RefCell::new([0u8; PAGE_SIZE]),
+            is_dirty: Cell::new(false),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Frame {
+    usage_count: u64,
+    buffer: Rc<Buffer>,
+}
+
+pub struct BufferPool {
+    buffers: Vec<Frame>,
+    next_victim_id: BufferId,
+}
+
+impl BufferPool {
+    pub fn new(pool_size: usize) -> Self {
+        let mut buffers = vec![];
+        
+    }
+}
